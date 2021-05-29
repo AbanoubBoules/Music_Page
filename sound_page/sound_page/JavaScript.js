cd5350d4e20c48e1dd0c80b7757a46e51fe84077
AbanoubBoules <Abanoub.Boules.Abdullah@gmail.com>
2021-05-29 05:16:03 -0700

    Sound page first commit

---
+++ sound_page/sound_page/JavaScript.js
@@ -0,0 +1,12 @@
+﻿var song = document.getElementById("song");
+var icon = document.getElementById("icon");
+icon.onclick = function () {
+    if (song.paused) {
+        song.play();
+        icon.src = "imgs/pause.png";
+    }
+    else {
+        song.pause();
+        icon.src = "imgs/play.png";
+    }
+}
